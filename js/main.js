57ec1f33da083fbb99fc3d12a768c30ebdbe77e4
José Luis Santana <yuniorglez@Mac-Yunior.local>
2018-03-07 11:00:20 +0000

    Solucionados ejercicios 1 2 y 3

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
-
-
 ////////////////////////////////////////
 /////////// Zona ejercicio 1 ///////////
 ////////////////////////////////////////
 
+/**
+ * Implementa la función ejercicio01 para que devuelva true ó false en el caso de que el email introducido en el input sea o no alguno de los siguientes: yunior.developer@hotmail.com, miguel@mrbug.es ó imanol@mercadona.com.
+ * @param {*} email 
+ */
+function ejercicio01(email) {
 
-function ejercicio01(email){
-    console.log(email);
+
+    if (email ===  'yunior.developer@hotmail.com' ||  email === 'miguel@mrbug.es' ||  email === 'imanol@mercadona') {
+        return true
+    } else {
+        return false;
+    }
+
+    // Otra posible solución
+    // return (email ===  'yunior.developer@hotmail.com' || email === 'miguel@mrbug.es' || email === 'imanol@mercadona');
 }
 
 
@@ -15,13 +25,34 @@ function ejercicio01(email){
 //////// Final zona ejercicio 1 ////////
 ////////////////////////////////////////
 
+
+
 ////////////////////////////////////////
 /////////// Zona ejercicio 2 ///////////
 ////////////////////////////////////////
 
-
-function ejercicio02(email){
-    console.log(email);
+/**
+ * Implementa la función ejercicio02 para que devuelva la siguiente frase dependiendo del correo introducido: 
+ * "El correo [correo] tiene [24] caracteres y en mayúsculas se quedaría así [CORREO]. Además [no] contiene [ninguna/2] letras M"
+ * @param {string} email 
+ */
+function ejercicio02(email) {
+    var minus = email.toLowerCase();
+    var numeroDeMs = 0;
+    for (var indice = 0; indice < email.length; indice++) {
+        if (minus[indice] === 'm') {
+            numeroDeMs++;
+        }
+    }
+    if (numeroDeMs === 0) {
+        return "El correo " + email + " tiene " + email.length + " caracteres y " +
+            "  en mayúsculas se quedaría así " + email.toUpperCase() + ". " +
+            "Además no contiene ninguna letras M";
+    } else {
+        return "El correo " + email + " tiene " + email.length + " caracteres y " +
+            "  en mayúsculas se quedaría así " + email.toUpperCase() + ". " +
+            "Además contiene " + numeroDeMs + " letras M";
+    }
 }
 
 
@@ -30,27 +61,65 @@ function ejercicio02(email){
 //////// Final zona ejercicio 2 ////////
 ////////////////////////////////////////
 
+
+
 ////////////////////////////////////////
 /////////// Zona ejercicio 3 ///////////
 ////////////////////////////////////////
 
+/**
+ * Implementa la función ejercicio03 para que devuelva la siguiente frase dependiendo del correo introducido: 
+ * "El correo [correo] pertenece al dominio [gmail.com] y tiene [X] caracteres sin contar el dominio ni el @. Además, el correo [no] contiene [ningún/X] número[s]"
+ * @param {string} email 
+ */
+function ejercicio03(email) {
+    /*
+    var posicionArroba = email.lastIndexOf('@');
+    var dominio = email.substring(posicionArroba+1);
+    var parteIzquierda = email.substring(0, posicionArroba );
+    */
 
-function ejercicio03(email){
-    console.log(email);
+    // Otra forma, pero primero hay que validar si el correo está bien formado 
+    var dominio = email.split('@')[1];
+    var parteIzquierda = email.split('@')[0];
+    var cuantosNumeros = cuentaNumeros(parteIzquierda);
+    var result = "El correo " + email + " pertenece al dominio " + dominio + " y tiene " + parteIzquierda.length + " caracteres sin contar el dominio ni el @.\n";
+    if (cuantosNumeros === 0) {
+        result += "Además, el correo no contiene ningún número";
+    } else if (cuantosNumeros === 1) {
+        result += "Además, el correo contiene un único número";
+    } else {
+        result += "Además, el correo contiene " + cuantosNumeros + " números";
+    }
+    return result;
 }
 
+function cuentaNumeros(frase){
+    if (!/\d/g.test(frase)) return 0
+    else return frase.match(/\d/g).length;
+
+    //Otra opcion MAS "FÁCIL"
+    /*
+    var cuenta = 0;
+    for (let i = 0; i < array.length; i++) {
+        const letra = frase[i];
+        if ("0123456789".includes(letra)) cuenta++
+    }
+    return cuenta;
+    */
+}
 
 
 ////////////////////////////////////////
 //////// Final zona ejercicio 3 ////////
 ////////////////////////////////////////
 
+
+
 ////////////////////////////////////////
 /////////// Zona ejercicio 4 ///////////
 ////////////////////////////////////////
-
-
-function ejercicio04(user){
+function ejercicio04(user) {
     console.log(user);
 }
 
@@ -60,12 +129,12 @@ function ejercicio04(user){
 //////// Final zona ejercicio 4 ////////
 ////////////////////////////////////////
 
+
+
 ////////////////////////////////////////
 /////////// Zona ejercicio 5 ///////////
 ////////////////////////////////////////
-
-
-function ejercicio05(user){
+function ejercicio05(user) {
     console.log(user);
 }
 
@@ -75,32 +144,19 @@ function ejercicio05(user){
 //////// Final zona ejercicio 5 ////////
 ////////////////////////////////////////
 
+
+
 ////////////////////////////////////////
 /////////// Zona ejercicio 6 ///////////
 ////////////////////////////////////////
-
-
-function ejercicio06(user){
-    console.log(user);
+function ejercicio06(user) {
+    return user;
 }
 
-
-
-////////////////////////////////////////
-//////// Final zona ejercicio 6 ////////
-////////////////////////////////////////
 
 ////////////////////////////////////////
 /////////// Zona ejercicio 7 ///////////
 ////////////////////////////////////////
-
-
-function ejercicio07(users){
-    console.log(users);
+function ejercicio06(user) {
+    return user;
 }
-
-
-
-////////////////////////////////////////
-//////// Final zona ejercicio 7 ////////
-////////////////////////////////////////
